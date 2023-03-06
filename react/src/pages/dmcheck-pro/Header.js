c0e2349175efb44414ba2b7ec88703407f44abf9
Adrian <aantosiak@icloud.com>
2023-03-06 16:44:21 +0100

    boxEnd link

--- react/src/pages/dmcheck-pro/Header.js
+++ react/src/pages/dmcheck-pro/Header.js
@@ -17,12 +17,22 @@ const Header = (props) => {
     e.preventDefault();
     props.workplaceLogout();
   }
+
+  const handleClickBoxEnd = (e) => {
+    e.preventDefault();
+    if (window.confirm("Czy na pewno chcesz zakończyć aktualny BOX? System poprosi o skan etykiety HYDRA.")) {
+      props.endBox();
+    }
+  }
   
   return (
     <header className="header">
       <img src={logo} alt="logo" className="header--logo" />
       <h1 className="header--title">DMCheck {props.workplaceName}</h1>
       <nav className="header--nav">
+        {props.endBox && (
+          <a href="/#" onClick={handleClickBoxEnd} className="header--link">Zakończ box</a>
+        )}
         <a href="/#" onClick={handleClickUserLogout} className="header--link">Operator</a>
         <a href="/#" onClick={handleClickArticleLogout} className="header--link">Artykuł</a>
         {!props.workplaceLogged && <a href="/#" onClick={handleClickWorkplaceLogout} className="header--link">Stanowisko</a>}
