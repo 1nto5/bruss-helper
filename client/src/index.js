2c70a15e147b1e3638bb6b82c334458ed580058d
Adrian <aantosiak@icloud.com>
2023-03-10 13:06:57 +0100

    user React route

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home'
+import User from './pages/user'
 import DmcheckPro from './pages/dmcheck-pro'
 import DmcheckMgmt from './pages/dmcheck-mgmt'
 import './assets/global.css'
@@ -12,6 +13,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/user" element={<User />} />
         <Route path="/dmcheck-pro" element={<DmcheckPro />} />
         <Route path="/dmcheck-mgmt" element={<DmcheckMgmt />} />
       </Routes>
