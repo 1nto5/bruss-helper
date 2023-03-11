e758486ff1f419d45c12f239b591c2e8927d85bb
Adrian <aantosiak@icloud.com>
2023-03-11 16:14:50 +0100

    user express router

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const routes = express.Router();
 
-import { registerUser  } from '../controllers/userRegistration.js';
+import { registerUser } from '../controllers/registerUser.js';
+import { confirmUser } from '../controllers/confirmUser.js';
 
 routes.post('/user/register', registerUser);
-// routes.post('/user/login', loginUser);s
+routes.get('/user/confirm', confirmUser);
 
 export default routes;
