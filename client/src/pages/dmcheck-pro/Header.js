ce82899c9cfc967f08c5324c309ca9821327b2df
Adrian <aantosiak@icloud.com>
2023-03-21 14:52:25 +0100

    tailwind

--- client/src/pages/dmcheck-pro/Header.js
+++ client/src/pages/dmcheck-pro/Header.js
@@ -26,23 +26,22 @@ const Header = (props) => {
   }
   
   return (
-    <header className='bg-black h-24 text-white flex items-center justify-between  px-5 py-3'>
-      <div className='flex items-center'>
-        <img src={logo} alt="logo" className='h-8 mr-2' />
-        <h1 className='text-lg font-bold'>DMCheck {props.workplaceName}</h1>
+    <nav className='flex items-center justify-between flex-wrap bg-gray-800 text-white p-6'>
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <img className="fill-current w-48 mr-2" src={logo} alt="logo" />
+        <span className="font-semibold text-2xl tracking-tight">DMCheck {props.workplaceName}</span>
       </div>
-
-      
+      <div>
         {props.endBox && (
-          <a href="/#" onClick={handleClickBoxEnd} className=''>Zakończ box</a>
+          <a href="/#" className='inline-block text-lg uppercase px-8 py-6 ml-auto leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickBoxEnd}>Zakończ</a>
         )}
-        <a href="/#" onClick={handleClickUserLogout} className=''>Operator</a>
-        <a href="/#" onClick={handleClickArticleLogout} className=''>Artykuł</a>
-        {!props.workplaceLogged && <a href="/#" onClick={handleClickWorkplaceLogout} className=''>Stanowisko</a>}
-
-
-      
-    </header>
+        <a href="/#" className='inline-block text-lg uppercase px-8 py-6 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickUserLogout}>Operator</a>
+        <a href="/#" className='inline-block text-lg uppercase px-8 py-6 ml-6 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickArticleLogout}>artykuł</a>
+        {!props.workplaceLogged && 
+          <a href="/#" className='inline-block text-lg uppercase px-8 py-6 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickWorkplaceLogout}>Stanowisko</a>
+        }
+      </div>
+    </nav>
   );
 };
 
