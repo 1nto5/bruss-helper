6322de5829d973916b058a15a10f01bdc91a32bb
Adrian <aantosiak@icloud.com>
2023-03-22 10:19:51 +0100

    tailwind

--- client/src/pages/dmcheck-pro/Header.js
+++ client/src/pages/dmcheck-pro/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../assets/logo.png'
+import { navButton, navButtonRed } from '../../assets/tailwind';
 
 const Header = (props) => {
   
@@ -26,19 +27,19 @@ const Header = (props) => {
   }
   
   return (
-    <nav className='flex items-center justify-between flex-wrap bg-gray-800 text-white p-6'>
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
+    <nav className='flex items-center justify-between flex-wrap bg-gray-200 shadow-2xl text-white p-6'>
+      <div className="flex items-center flex-shrink-0 text-black mr-6">
         <img className="fill-current w-48 mr-2" src={logo} alt="logo" />
         <span className="font-semibold text-2xl tracking-tight">DMCheck {props.workplaceName}</span>
       </div>
       <div>
         {props.endBox && (
-          <a href="/#" className='inline-block text-lg uppercase px-8 py-6 ml-auto leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickBoxEnd}>Zakończ</a>
+          <a href="/#" className={navButtonRed} onClick={handleClickBoxEnd}>zakończ box</a>
         )}
-        <a href="/#" className='inline-block text-lg uppercase px-8 py-6 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickUserLogout}>Operator</a>
-        <a href="/#" className='inline-block text-lg uppercase px-8 py-6 ml-6 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickArticleLogout}>artykuł</a>
+        <a href="/#" className={navButton} onClick={handleClickUserLogout}>wyloguj operatora</a>
+        <a href="/#" className={navButton} onClick={handleClickArticleLogout}>zmień artykuł</a>
         {!props.workplaceLogged && 
-          <a href="/#" className='inline-block text-lg uppercase px-8 py-6 leading-none border rounded text-white border-white hover:border-transparent hover:text-black hover:bg-white' onClick={handleClickWorkplaceLogout}>Stanowisko</a>
+          <a href="/#" className={navButton} onClick={handleClickWorkplaceLogout}>zmień stanowisko</a>
         }
       </div>
     </nav>
