68fc7dab35aed69da66ef8066d41373b50439084
Adrian <aantosiak@icloud.com>
2023-03-27 16:41:20 +0200

    tailwind

--- client/src/pages/dmcheck-mgmt/Chooser.js
+++ client/src/pages/dmcheck-mgmt/Chooser.js
@@ -1,122 +1,177 @@
-import React, { useState, useMemo } from 'react'
-import production from "../../data/production"
+import React, { useState, useMemo } from "react";
+import production from "../../data/production";
+import { BoxSeparator } from "../../components/StatusElements";
 
 // TODO after selecting the status restart the article
 
 const Chooser = (props) => {
-    const workplaces = useMemo(() => production.map(workplace => Object.keys(workplace)[0]), [production]);
-    const [workplaceArticles, setWorkplaceArticles] = useState([]);
-    
-    const handleWorkplaceChange = (e) => {
-      const selectedWorkplace = e.target.value;
-      props.handleWorkplaceChange(selectedWorkplace);
-      const selectedWorkplaceData = production.find(w => Object.keys(w)[0] === selectedWorkplace);
-      if (selectedWorkplaceData) {
-        const articles = selectedWorkplaceData[selectedWorkplace].articles;
-        setWorkplaceArticles(Object.entries(articles));
-      }
-    };
-    
-    const handleArticleChange = (e) => {
-        props.handleArticleChange(e.target.value);
-    }
-    
-    const handleStatusChange = (e) => {
-      props.handleStatusChange(e.target.value);
-    }
-    
-    const handleOperatorInput = (e) => {
-        props.handleOperatorInput(e.target.value);
-    }
+  const workplaces = useMemo(
+    () => production.map((workplace) => Object.keys(workplace)[0]),
+    [production]
+  );
+  const [workplaceArticles, setWorkplaceArticles] = useState([]);
 
-    const handleStartDateChange = (e) => {
-        props.handleStartDateChange(e.target.value);
+  const handleWorkplaceChange = (e) => {
+    const selectedWorkplace = e.target.value;
+    props.handleWorkplaceChange(selectedWorkplace);
+    const selectedWorkplaceData = production.find(
+      (w) => Object.keys(w)[0] === selectedWorkplace
+    );
+    if (selectedWorkplaceData) {
+      const articles = selectedWorkplaceData[selectedWorkplace].articles;
+      setWorkplaceArticles(Object.entries(articles));
     }
-      
-    const handleEndDateChange = (e) => {
-        props.handleEndDateChange(e.target.value);
-    }
-      
-    const handleDmcOrBatchInput = (e) => {
-        props.handleDmcOrBatchInput(e.target.value);
-    }
+  };
 
-    return (
-        <div className='mgmt-chooser'>
-            <div className='mgmt-chooser--box'>
-                <div className='mgmt-chooser--box--row'>
-                    <select className='mgmt-chooser--row--select' value={props.workplace || "default"} onChange={handleWorkplaceChange}>
-                        <option value="default" disabled>stanowisko</option>
-                        {workplaces.map(workplace => (
-                            <option key={workplace} value={workplace}>
-                            {workplace}
-                        </option>
-                        ))}
-                        <option value="">wyczyść</option>
-                    </select>
-                </div>
-                <div className='mgmt-chooser--box--row'>
-                    <select className='mgmt-chooser--row--select' id="article" value={props.article || "default"} onChange={handleArticleChange}>
-                        <option value="default" disabled>artykuł</option>
-                        {workplaceArticles.length > 0 
-                        ? workplaceArticles.map(([articleNumber, article]) => (
-                            <option key={articleNumber} value={props.articleNumber}>
-                                {articleNumber}
-                            </option>
-                            ))
-                            : <option value="default" disabled>brak stanowiska</option>
-                        }
-                        <option value="">wyczyść</option>
-                        
-                    </select>
-                </div>
-                <div className='mgmt-chooser--box--row'>
-                <select className='mgmt-chooser--row--select' value={props.status || "default"} onChange={handleStatusChange}>
-                    <option value="default" disabled>status</option>
-                    <option key='box' value='0'>box</option>
-                    <option key='paleta' value='1'>paleta</option>
-                    <option key='magazyn' value='2'>magazyn</option>
-                    <option value="">wyczyść</option>
-                </select>
-            </div>
-            </div>
-            <div className="mgmt-chooser--separator"></div>
-            <div className='mgmt-chooser--box'> 
-  
-            <div className='mgmt-chooser--box--row'>
-                <input 
-                    className='mgmt-chooser--row--input' 
-                    value={props.operator} 
-                    onChange={handleOperatorInput}
-                    placeholder="operator"
-                >
-                </input>
-            </div>
-            <div className='mgmt-chooser--box--row'>
-                    <input 
-                        className='mgmt-chooser--row--input' 
-                        value={props.dmcInput} 
-                        onChange={handleDmcOrBatchInput}
-                        placeholder="dmc / hydra / paleta"
-                    >
-                    </input>
-                </div>
-            </div>
-            <div className="mgmt-chooser--separator"></div>
-                <div className='mgmt-chooser--box'>
-                    <div className='mgmt-chooser--box--row'>
-                        <label className='mgmt-chooser--row--label'  htmlFor="start-date">od:</label>
-                        <input className='mgmt-chooser--row--input' type="datetime-local" id="start-date" value={props.startDate} onChange={handleStartDateChange} /> 
-                    </div>
-                </div>
-            <div className="mgmt-chooser--separator"></div>
-            <div className='mgmt-chooser--box'>
-            <div className='mgmt-chooser--box--row'>
-                <label className='mgmt-chooser--row--label' htmlFor="end-date">do:</label>
-                <input className='mgmt-chooser--row--input' type="datetime-local" id="end-date" value={props.endDate} onChange={handleEndDateChange} />
-            </div>
-            </div>
+  const handleArticleChange = (e) => {
+    props.handleArticleChange(e.target.value);
+  };
+
+  const handleStatusChange = (e) => {
+    props.handleStatusChange(e.target.value);
+  };
+
+  const handleOperatorInput = (e) => {
+    props.handleOperatorInput(e.target.value);
+  };
+
+  const handleStartDateChange = (e) => {
+    props.handleStartDateChange(e.target.value);
+  };
+
+  const handleEndDateChange = (e) => {
+    props.handleEndDateChange(e.target.value);
+  };
+
+  const handleDmcOrBatchInput = (e) => {
+    props.handleDmcOrBatchInput(e.target.value);
+  };
+
+  return (
+    <div className="flex h-40 flex-row items-center justify-between bg-gray-50 shadow-lg">
+      <div className="ml-auto mr-auto box-border text-center">
+        <div>
+          <select
+            className="mb-2 h-8 w-48 text-gray-700"
+            value={props.workplace || "default"}
+            onChange={handleWorkplaceChange}
+          >
+            <option value="default" disabled>
+              stanowisko
+            </option>
+            {workplaces.map((workplace) => (
+              <option key={workplace} value={workplace}>
+                {workplace}
+              </option>
+            ))}
+            <option value="">wyczyść</option>
+          </select>
         </div>
-    )
-}
-export default Chooser
+        <div>
+          <select
+            className="mb-2 h-8 w-48 text-gray-700"
+            id="article"
+            value={props.article || "default"}
+            onChange={handleArticleChange}
+          >
+            <option value="default" disabled>
+              artykuł
+            </option>
+            {workplaceArticles.length > 0 ? (
+              workplaceArticles.map(([articleNumber, article]) => (
+                <option key={articleNumber} value={props.articleNumber}>
+                  {articleNumber}
+                </option>
+              ))
+            ) : (
+              <option value="default" disabled>
+                brak stanowiska
+              </option>
+            )}
+            <option value="">wyczyść</option>
+          </select>
+        </div>
+        <div>
+          <select
+            className="h-8 w-48 text-gray-700"
+            value={props.status || "default"}
+            onChange={handleStatusChange}
+          >
+            <option value="default" disabled>
+              status
+            </option>
+            <option key="box" value="0">
+              box
+            </option>
+            <option key="paleta" value="1">
+              paleta
+            </option>
+            <option key="magazyn" value="2">
+              magazyn
+            </option>
+            <option value="">wyczyść</option>
+          </select>
+        </div>
+      </div>
+      <BoxSeparator />
+      <div
+        className="box-border flex w-1/4 flex-col items-center justify-center p-5 text-center
+"
+      >
+        <div>
+          <input
+            className="mb-2 h-8 font-sans font-normal placeholder-gray-700"
+            value={props.operator}
+            onChange={handleOperatorInput}
+            placeholder="operator"
+          ></input>
+        </div>
+        <div>
+          <input
+            className="font-norma gray-700 h-8 font-sans placeholder-gray-700"
+            value={props.dmcInput}
+            onChange={handleDmcOrBatchInput}
+            placeholder="dmc / hydra / paleta"
+          ></input>
+        </div>
+      </div>
+      <BoxSeparator />
+      <div
+        className="box-border flex w-1/4 flex-col items-center justify-center p-5 text-center
+"
+      >
+        <div>
+          <label className="mx-2 font-thin text-gray-700" htmlFor="start-date">
+            od:
+          </label>
+          <input
+            className="mx-1 h-8 font-normal"
+            type="datetime-local"
+            id="start-date"
+            value={props.startDate}
+            onChange={handleStartDateChange}
+          />
+        </div>
+      </div>
+      <BoxSeparator />
+      <div
+        className="box-border flex w-1/4 flex-col items-center justify-center p-5 text-center
+"
+      >
+        <div>
+          <label className="mx-2 font-thin text-gray-700" htmlFor="end-date">
+            do:
+          </label>
+          <input
+            className="mx-1 h-8 font-sans font-normal"
+            type="datetime-local"
+            id="end-date"
+            value={props.endDate}
+            onChange={handleEndDateChange}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Chooser;
--- client/src/pages/dmcheck-mgmt/Header.js
+++ client/src/pages/dmcheck-mgmt/Header.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import logo from '../../assets/logo.png'
+import React from "react";
+import logo from "../../assets/logo.png";
+import { HeaderLinkButton } from "../../components/Buttons";
 
 const Header = (props) => {
-  
   const handleClickSkip = (e) => {
     e.preventDefault();
     if (window.confirm("Czy na pewno chcesz pominąć zaznaczone pozycje?")) {
       props.clickSkip();
     }
-  }
-  
+  };
+
   return (
-    <header className="header">
-      <img src={logo} alt="logo" className="header--logo" />
-      <h1 className="header--title">DMCheck MGMT</h1>
-      <nav className="header--nav">
-        <a href="/#" onClick={handleClickSkip} className="header--link">Pomiń</a>
-      </nav>
-    </header>
+    // <header className="header">
+    //   <img src={logo} alt="logo" className="header--logo" />
+    //   <h1 className="header--title">DMCheck MGMT</h1>
+    //   <nav className="header--nav">
+    //     <a href="/#" onClick={handleClickSkip} className="header--link">Pomiń</a>
+    //   </nav>
+    // </header>
+    <nav className="flex flex-wrap items-center justify-between bg-gray-800 p-4">
+      <div className="shad mr-6 flex flex-shrink-0 items-center text-gray-50">
+        <img className="ml-4 mr-6 w-48 fill-current" src={logo} alt="logo" />
+        <span className="text-2xl font-bold tracking-tight">DMCheck MGMT</span>
+      </div>
+      <div>
+        <HeaderLinkButton text="pomiń" onClick={handleClickSkip} />
+      </div>
+    </nav>
   );
 };
 
