3ada09dac6a48fc52656b51de817f3e340f7478b
Adrian <aantosiak@icloud.com>
2023-03-30 13:06:07 +0200

    route fix

--- controllers/dmcheckPro.js
+++ controllers/dmcheckPro.js
@@ -1,57 +1,82 @@
-import countDocuments from '../models/dmc.js';
+import countDocuments from "../models/dmc.js";
 
 export const countDmc = async (req, res) => {
-    try {
-        const count = await countDocuments.countDocuments({ status: req.query.status, article: req.query.article });
-        res.json({ message: count });
-    } catch (error) {
-        return res.json({ message: error });
-    }
+  try {
+    const count = await countDocuments.countDocuments({
+      status: req.query.status,
+      article: req.query.article,
+    });
+    res.json({ message: count });
+  } catch (error) {
+    return res.json({ message: error });
   }
-
-export const saveDmc = async (req, res) => {
-    try {
-        const data = req.body;
-        const existingData = await countDocuments.findOne({ dmc: data.dmc });
-        if (existingData) return res.json({ message: 'exists' });
-        const newDmc = new countDocuments({ status: 0,  ...data, dmc_time: new Date() });
-        await newDmc.save();
-        res.json({ message: 'saved' });
-    } catch (error) {
-        return res.json({ message: error });
-    }
 };
 
+export const saveDmc = async (req, res) => {
+  try {
+    const data = req.body;
+    const existingData = await countDocuments.findOne({ dmc: data.dmc });
+    if (existingData) return res.json({ message: "exists" });
+    const newDmc = new countDocuments({
+      status: 0,
+      ...data,
+      dmc_time: new Date(),
+    });
+    await newDmc.save();
+    res.json({ message: "saved" });
+  } catch (error) {
+    return res.json({ message: error });
+  }
+};
 
 export const saveHydra = async (req, res) => {
-    try {
-        const data = req.body;
-        const existingData = await countDocuments.findOne({ workplace: data.workplace, article: data.article, hydra_batch: data.hydra_batch });
-        console.log(existingData)
-        if (existingData) return res.json({ message: 'exists' });
-        await countDocuments.updateMany(
-            { status: 0, article: data.article, workplace: data.workplace }, 
-            { $set: { status: 1, hydra_batch: data.hydra_batch, hydra_operator: data.hydra_operator, hydra_time: new Date() } }
-        );
-        res.json({ message: 'saved' });
-    } catch (error) {
-        return res.json({ message: error });
-    }
+  try {
+    const data = req.body;
+    const existingData = await countDocuments.findOne({
+      workplace: data.workplace,
+      article: data.article,
+      hydra_batch: data.hydra_batch,
+    });
+    console.log(existingData);
+    if (existingData) return res.json({ message: "exists" });
+    await countDocuments.updateMany(
+      { status: 0, article: data.article, workplace: data.workplace },
+      {
+        $set: {
+          status: 1,
+          hydra_batch: data.hydra_batch,
+          hydra_operator: data.hydra_operator,
+          hydra_time: new Date(),
+        },
+      }
+    );
+    res.json({ message: "saved" });
+  } catch (error) {
+    return res.json({ message: error });
   }
+};
 
-  export const savePallet = async (req, res) => {
-    try {
-        const data = req.body;
-        const existingData = await countDocuments.findOne({ pallet_batch: data.pallet_batch });
-        if (existingData) return res.json({ message: 'exists' });
-        await countDocuments.updateMany(
-            { status: 1, article: data.article, workplace: data.workplace }, 
-            { $set: { status: 2, pallet_batch: data.pallet_batch, pallet_operator: data.pallet_operator, pallet_time: new Date() }}
-        );
-        res.json({ message: 'saved' });
-    } catch (error) {
-        return res.json({ message: error });
-    }
+export const savePallet = async (req, res) => {
+  try {
+    const data = req.body;
+    const existingData = await countDocuments.findOne({
+      pallet_batch: data.pallet_batch,
+    });
+
+    if (existingData) return res.json({ message: "exists" });
+    await countDocuments.updateMany(
+      { status: 1, article: data.article, workplace: data.workplace },
+      {
+        $set: {
+          status: 2,
+          pallet_batch: data.pallet_batch,
+          pallet_operator: data.pallet_operator,
+          pallet_time: new Date(),
+        },
+      }
+    );
+    res.json({ message: "saved" });
+  } catch (error) {
+    return res.json({ message: error });
   }
-
-
+};
--- routes/dmcheckPro.js
+++ routes/dmcheckPro.js
@@ -1,11 +1,16 @@
-import express from 'express';
+import express from "express";
 const routes = express.Router();
 
-import { countDmc, saveDmc, saveHydra  } from '../controllers/dmcheckPro.js';
+import {
+  countDmc,
+  saveDmc,
+  saveHydra,
+  savePallet,
+} from "../controllers/dmcheckPro.js";
 
-routes.get('/dmcheck-pro/count-dmc', countDmc);
-routes.post('/dmcheck-pro/save-dmc', saveDmc);
-routes.post('/dmcheck-pro/save-hydra', saveHydra);
-routes.post('/dmcheck-pro/save-pallet', saveHydra);
+routes.get("/dmcheck-pro/count-dmc", countDmc);
+routes.post("/dmcheck-pro/save-dmc", saveDmc);
+routes.post("/dmcheck-pro/save-hydra", saveHydra);
+routes.post("/dmcheck-pro/save-pallet", savePallet);
 
 export default routes;
