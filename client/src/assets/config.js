cf2e6360fcaa6a6258658b57a0741caa4a7feb9a
Adrian <aantosiak@icloud.com>
2023-03-30 11:27:44 +0200

    pro config

--- client/src/assets/config.js
+++ client/src/assets/config.js
@@ -1,3 +1,2 @@
-export const API_URL = "http://localhost:4000"; // DEVELOPMENT
-// export const API_URL = ""; // PRODUCTION
-
+// export const API_URL = "http://localhost:4000"; // DEVELOPMENT
+export const API_URL = ""; // PRODUCTION
