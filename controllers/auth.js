2218ff0ef053c56f293f91dd34124306c52a0bd1
Adrian <aantosiak@icloud.com>
2023-03-30 07:16:43 +0200

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,42 +2,50 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+// Register a new user
 export const register = async (req, res) => {
   const { email, password } = req.body;
 
+  // Check if user with given email already exists
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
     return res.status(409).json({ message: "User already exists" });
   }
 
+  // Create a new user and save it to the database
   const user = new User({ email, password });
   await user.save();
 
   res.json({ message: "User registered successfully" });
 };
 
+// Log in a user and issue a JWT token
 export const login = async (req, res) => {
   const { email, password } = req.body;
 
+  // Find the user with the given email
   const user = await User.findOne({ email });
 
   if (!user) {
     return res.status(404).json({ message: "User not found" });
   }
 
+  // Check if the provided password matches the user's password
   const passwordMatch = await bcrypt.compare(password, user.password);
 
   if (!passwordMatch) {
     return res.status(401).json({ message: "Invalid password" });
   }
 
+  // If the password is correct, create a JWT token and send it to the client
   const payload = { id: user.id, email: user.email };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "8h" });
 
   res.json({ token });
 };
 
+// Fetch the management access level for a user with a valid JWT token
 export const fetchMgmtAccess = async (req, res) => {
   try {
     // Get the token from the request headers
@@ -59,10 +67,27 @@ export const fetchMgmtAccess = async (req, res) => {
       return res.status(401).json({ msg: "User not found" });
     }
 
-    // Return the dmcheckMgmtAccess value from the user object
+    // Return the management access level for the user
     res.json({ dmcheckMgmtAccess: user.dmcheckMgmtAccess });
   } catch (err) {
-    console.error(err.message);
     res.status(500).send("Server Error");
+  }
+};
+
+// Check if a JWT token has expired
+export const isTokenExpired = (req, res) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+
+  try {
+    // Verify the token to see if it has expired
+    jwt.verify(token, process.env.JWT_SECRET);
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(401).json({ msg: "Token expired" });
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { login, register, fetchMgmtAccess } from "../controllers/auth.js";
+import {
+  login,
+  register,
+  fetchMgmtAccess,
+  isTokenExpired,
+} from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/auth/register", register);
 router.post("/auth/login", login);
+router.get("/auth/is-token-expired", isTokenExpired);
 router.get("/auth/mgmt-access", fetchMgmtAccess);
 
 export default router;
