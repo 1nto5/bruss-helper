966d7e6028fe7f79acfc9044302b6e173050e361
Adrian <aantosiak@icloud.com>
2023-03-30 11:22:16 +0200

    shadow fix

--- client/src/pages/dmcheck-mgmt/Header.js
+++ client/src/pages/dmcheck-mgmt/Header.js
@@ -26,7 +26,7 @@ const Header = (props) => {
   };
 
   return (
-    <nav className="flex flex-wrap items-center justify-between bg-gray-800 p-4">
+    <nav className="flex flex-wrap items-center justify-between bg-gray-800 p-4 shadow-lg">
       <div className="shad mr-6 flex flex-shrink-0 items-center text-gray-50">
         <img className="ml-4 mr-6 w-48 fill-current" src={logo} alt="logo" />
         <span className="text-2xl font-bold tracking-tight">DMCheck MGMT</span>
