c06abc2dce31c6c04640a739f31a776fd8ee2591
Adrian <aantosiak@icloud.com>
2023-03-31 18:37:23 +0200

    fix route

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post("/auth/register", register);
 router.post("/auth/login", login);
-router.get("/auth/is-token-expired", isTokenValid);
+router.get("/auth/is-token-valid", isTokenValid);
 router.get("/auth/mgmt-access", fetchMgmtAccess);
 
 export default router;
