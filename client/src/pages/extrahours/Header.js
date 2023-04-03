3592793bef8046eb1c6425d6ecf8c4fcc6bd25ca
Adrian <aantosiak@icloud.com>
2023-04-03 20:27:15 +0200

    extra hours header

---
+++ client/src/pages/extrahours/Header.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from "react";
+import logo from "../../assets/logo.png";
+import { HeaderLinkButton } from "../../components/Buttons";
+import LoginModal from "../../components/AuthModal";
+import { AuthContext } from "../../contexts/AuthContext";
+
+const Header = (props) => {
+  const [showLoginModal, setShowLoginModal] = useState(false);
+  const { isLoggedIn, logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    if (window.confirm("Czy na pewno chcesz się wylogować?")) {
+      logout();
+    }
+  };
+
+  const toggleLoginModal = () => {
+    setShowLoginModal(!showLoginModal);
+  };
+
+  return (
+    <nav className="flex flex-wrap items-center justify-between bg-gray-800 p-4 shadow-lg">
+      <div className="shad mr-6 flex flex-shrink-0 items-center text-gray-50">
+        <img className="ml-4 mr-6 w-48 fill-current" src={logo} alt="logo" />
+        <span className="text-2xl font-bold tracking-tight">DMCheck MGMT</span>
+      </div>
+      <div>
+        {isLoggedIn ? (
+          <>
+            <HeaderLinkButton text="wyloguj" onClick={handleLogout} />
+          </>
+        ) : (
+          <HeaderLinkButton text="zaloguj" onClick={toggleLoginModal} />
+        )}
+      </div>
+      {showLoginModal && <LoginModal onClose={toggleLoginModal} />}
+    </nav>
+  );
+};
+
+export default Header;
