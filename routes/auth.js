bf1b428dcc8ea0d0b5f817ad72c75a29981abb26
Adrian <aantosiak@icloud.com>
2023-04-05 11:45:41 +0200

    user id route

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ import {
   fetchMgmtAccess,
   isTokenValid,
   getUsernameFromToken,
+  getUserIdFromToken,
 } from "../controllers/auth.js";
 
 const router = express.Router();
@@ -14,5 +15,6 @@ router.post("/auth/login", login);
 router.get("/auth/is-token-valid", isTokenValid);
 router.get("/auth/mgmt-access", fetchMgmtAccess);
 router.get("/auth/extract-username", getUsernameFromToken);
+router.get("/auth/extract-userid", getUserIdFromToken);
 
 export default router;
