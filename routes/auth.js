d96bd6fc47f13018b01510494cfbfae92da642c5
Adrian <aantosiak@icloud.com>
2023-04-05 12:33:19 +0200

    fetching supervisors route

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ import {
   isTokenValid,
   getUsernameFromToken,
   getUserIdFromToken,
+  getExtraHoursSupervisors,
 } from "../controllers/auth.js";
 
 const router = express.Router();
@@ -16,5 +17,6 @@ router.get("/auth/is-token-valid", isTokenValid);
 router.get("/auth/mgmt-access", fetchMgmtAccess);
 router.get("/auth/extract-username", getUsernameFromToken);
 router.get("/auth/extract-userid", getUserIdFromToken);
+router.get("/auth/extrahours-supervisors", getExtraHoursSupervisors);
 
 export default router;
