3f623576f77b49308df3794ef993f8423ac50394
Adrian <aantosiak@icloud.com>
2023-04-26 11:32:03 +0200

    cors to use in local network

--- app.js
+++ app.js
@@ -1,57 +1,57 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import dmcheckMgmtRoutes from "./routes/dmcheckMgmt.js";
-import dmcheckProRoutes from "./routes/dmcheckPro.js";
-import authRoutes from "./routes/auth.js";
-import extraHoursRoutes from "./routes/extraHours.js";
-import inventoryRoutes from "./routes/inventory.js";
-import dotenv from "dotenv";
-import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import dmcheckMgmtRoutes from './routes/dmcheckMgmt.js'
+import dmcheckProRoutes from './routes/dmcheckPro.js'
+import authRoutes from './routes/auth.js'
+import extraHoursRoutes from './routes/extraHours.js'
+import inventoryRoutes from './routes/inventory.js'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const app = express();
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.json())
 
-if (process.env.NODE_ENV === "production") {
-  dotenv.config({ path: ".env.production" });
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  console.log("production");
-  app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' })
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  console.log('production')
+  app.use(express.static(path.join(__dirname, 'client/build')))
   app.use(
-    "/",
+    '/',
     dmcheckMgmtRoutes,
     dmcheckProRoutes,
     authRoutes,
     extraHoursRoutes,
     inventoryRoutes
-  );
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
+  )
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'))
+  })
 } else {
-  dotenv.config({ path: ".env.development" });
-  console.log("development");
-  app.use(cors({ origin: "http://localhost:3000" }));
+  dotenv.config({ path: '.env.development' })
+  console.log('development')
+  app.use(cors({ origin: '*' }))
   app.use(
-    "/",
+    '/',
     dmcheckMgmtRoutes,
     dmcheckProRoutes,
     authRoutes,
     extraHoursRoutes,
     inventoryRoutes
-  );
+  )
 }
 
-mongoose.set("strictQuery", false);
-const dbUri = process.env.DB_URI;
-mongoose.connect(dbUri, { useNewUrlParser: true });
+mongoose.set('strictQuery', false)
+const dbUri = process.env.DB_URI
+mongoose.connect(dbUri, { useNewUrlParser: true })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Express server listening on port ${process.env.PORT}`);
-});
+  console.log(`Express server listening on port ${process.env.PORT}`)
+})
 
 // NODE_ENV=production node app.js
 // yarn nodemon
