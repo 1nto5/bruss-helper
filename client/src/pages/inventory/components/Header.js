9e30b5e9edec840ca709f6518459fdc599c1f04a
Adrian <aantosiak@icloud.com>
2023-05-23 14:28:31 +0200

    close card

--- client/src/pages/inventory/components/Header.js
+++ client/src/pages/inventory/components/Header.js
@@ -1,12 +1,12 @@
-import React, { useState, useContext } from "react";
-import logo from "../../../assets/logo.png";
-import { HeaderLinkButton } from "../../../components/Buttons";
-import Modal from "./Modal";
-import { Context } from "../Context";
+import React, { useState, useContext } from 'react'
+import logo from '../../../assets/logo.png'
+import { HeaderLinkButton } from '../../../components/Buttons'
+import Modal from './Modal'
+import { Context } from '../Context'
 
 const Header = (props) => {
-  const [showCardChooserModal, setShowCardChooserModal] = useState(false);
-  const { cardNumber, resetContext } = useContext(Context);
+  const [showCardChooserModal, setShowCardChooserModal] = useState(false)
+  const { cardNumber, closeCard } = useContext(Context)
 
   const handleEndCard = () => {
     if (
@@ -14,13 +14,13 @@ const Header = (props) => {
         `Czy na pewno chcesz zakończyć pracę z kartą numer: ${cardNumber}?`
       )
     ) {
-      resetContext();
+      closeCard.mutate()
     }
-  };
+  }
 
   const toggleModal = () => {
-    setShowCardChooserModal(!showCardChooserModal);
-  };
+    setShowCardChooserModal(!showCardChooserModal)
+  }
 
   return (
     <nav className="flex flex-wrap items-center justify-between bg-gray-800 p-4 shadow-lg">
@@ -39,7 +39,7 @@ const Header = (props) => {
       </div>
       {showCardChooserModal && <Modal onClose={toggleModal} />}
     </nav>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
